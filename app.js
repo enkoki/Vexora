efb16dcc324777a727ff4987671a5a63c8771345
Rijan Dahal <reasondahal719@gmail.com>
2026-03-18 01:04:48 +0545

    Feat: Home Page

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import { updateSlider } from "./lib/slider.js";
+import { updateCountdown } from "./lib/countdown.js";
+import { renderProducts } from "./lib/product.js";
 
 updateSlider()
+updateCountdown();
+renderProducts();
