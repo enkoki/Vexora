9e05f382c222d4b473edc27b016e614e3b49b7d1
Rijan Dahal <reasondahal719@gmail.com>
2026-04-02 10:30:02 +0545

    feat: login page

---
+++ routes/Login/validation.js
@@ -0,0 +1,44 @@
+const form = document.getElementById("loginForm");
+const emailOrPhone = document.getElementById("emailOrPhone");
+const password = document.getElementById("password");
+
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let isValid = true;
+
+    resetError(emailOrPhone, emailError);
+    resetError(password, passwordError);
+
+    const value = emailOrPhone.value.trim();
+    const pass = password.value.trim();
+
+    if (!value.includes("@") || !value.includes(".")) {
+        showError(emailOrPhone, emailError, "Please enter a valid email");
+        isValid = false;
+    }
+
+    if (pass.length <= 8) {
+        showError(password, passwordError, "Password must be greater than 8 characters");
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Login successful!");
+    }
+});
+
+function showError(input, errorElement, message) {
+    errorElement.textContent = message;
+    errorElement.classList.remove("hidden");
+    input.classList.add("border-red-500");
+}
+
+function resetError(input, errorElement) {
+    errorElement.textContent = "";
+    errorElement.classList.add("hidden");
+    input.classList.remove("border-red-500");
+}
